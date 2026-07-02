6baeb52ee17816847fbc485cf6fb1356ebee7324
Akarsh Reddy Eathamukkala <akarsh16reddy@gmail.com>
2026-07-02 00:53:31 -0400

    add asyncpg and code for postgres

---
+++ asyncio_database_drivers/5_4_insertin_brands.py
@@ -0,0 +1,28 @@
+import asyncio
+from typing import List
+
+import asyncpg
+from asyncpg import Record
+
+
+async def main():
+    connection = await asyncpg.connect(
+        host="127.0.0.1",
+        port=5432,
+        user="postgres",
+        database="products",
+        password="password",
+    )
+    await connection.execute("INSERT INTO brand VALUES(DEFAULT, 'Levis')")
+    await connection.execute("INSERT INTO brand VALUES(DEFAULT, 'Seven')")
+
+    brand_query = "SELECT brand_id, brand_name from brand"
+    results: List[Record] = await connection.fetch(brand_query)
+
+    for brand in results:
+        print(f"id: {brand['brand_id']}, name: {brand['brand_name']}")
+
+    await connection.close()
+
+
+asyncio.run(main())
